eb607b6fd173d00fd3fd0f631042d32c4c9c4121
u0995526 <u0995526@MLPC-CL530>
2022-08-31 20:45:17 -0600

    Setup

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -1,7 +1,23 @@
-﻿namespace FormulaEvaluator
+﻿using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator
 {
     public static class Evaluator
     {
-        // TODO: Follow the PS1 instructions
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            foreach(string substring in substrings)
+            {
+                if(substring is int)
+                {
+
+                }
+                int varVal = variableEvaluator(substring);
+            }
+            return 0;
+        }
     }
 }
